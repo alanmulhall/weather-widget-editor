5a95ef9cd7fe0de7ad82beb4d8f11ccaf02ed8f5
alan mulhall <alanmulhall@gmail.com>
2016-11-30 13:00:02 +0700

    Removed some unneeded extra spacing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 import widgets from './widgets';
-import { reducer as formReducer } from 'redux-form';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
   widgets,
